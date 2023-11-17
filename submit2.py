0afac13728d45654e1debe3656c3d0530dca8c2f
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 18:26:03 +0900

    Update submit2.py
    베르누이 나이브 베이즈 분류

--- submit2.py
+++ submit2.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+# 베르누이 나이브 베이즈 분류 사용
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import BernoulliNB
@@ -16,19 +11,11 @@ y_train = train['sentiment']
 
 X_test = test['text']
 
-
-# In[ ]:
-
-
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train = vectorizer.fit_transform(X_train).toarray()
 
 bnb = BernoulliNB()
 bnb.fit(X_train, y_train)
-
-
-# In[ ]:
-
 
 X_test = vectorizer.transform(X_test).toarray()
 preds = bnb.predict(X_test)
@@ -36,10 +23,6 @@ preds = bnb.predict(X_test)
 submit2 = pd.read_csv('C:/Users/sohnp/Downloads/open (1)/sample_submission.csv')
 submit2['sentiment'] = preds
 submit2.head()
-
-
-# In[ ]:
-
 
 submit2.to_csv('C:/Users/sohnp/baseline_submit6.csv', index=False)
 print('Done')
