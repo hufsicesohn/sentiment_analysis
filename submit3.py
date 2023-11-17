e89184d3128f7d9235106d8a6d385f15a3075559
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 18:26:42 +0900

    Update submit3.py
    랜덤 포레스트

--- submit3.py
+++ submit3.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+# 랜덤 포레스트 사용
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
@@ -16,19 +11,11 @@ y_train = train['sentiment']
 
 X_test = test['text']
 
-
-# In[ ]:
-
-
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train = vectorizer.fit_transform(X_train).toarray()
 
 clf = RandomForestClassifier(n_estimators=100,min_samples_split=5)
 clf.fit(X_train,y_train)
-
-
-# In[ ]:
-
 
 X_test = vectorizer.transform(X_test).toarray()
 pred = clf.predict(X_test)
@@ -37,16 +24,8 @@ submit3 = pd.read_csv('C:/Users/sohnp/Downloads/open (1)/sample_submission.csv')
 submit3['sentiment'] = preds
 submit3.head()
 
-
-# In[ ]:
-
-
 submit3.to_csv('C:/Users/sohnp/baseline_submit7.csv', index=False)
 print('Done')
-
-
-# In[ ]:
-
 
 submit3 = pd.read_csv('C:/Users/sohnp/baseline_submit7.csv')
 
