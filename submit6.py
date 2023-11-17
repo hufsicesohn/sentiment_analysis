390bf3635b80b13433edaa9a4a87f0611132b5c0
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 18:37:49 +0900

    Update and rename submit6,7.py to submit6.py
    pipeline + 로지스틱 회귀

--- submit6.py
+++ submit6.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+# pipeline사용, 로지스틱회귀 사용
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 import re
@@ -19,19 +14,13 @@ y_train = train['sentiment']
 X_test = test['text']
 
 
-# In[ ]:
-
-
 for i in range(32000):
   X_train[i] = re.sub('[^a-zA-Z]', ' ', X_train[i])
 
 for i in range(32000):
   X_test[i] = re.sub('[^a-zA-Z]', ' ', X_test[i])
 
-
-# In[ ]:
-
-
+# 단어를 벡터로 변환하는 작업과 로지스틱 회귀를 한 번에 묶어주는 pipleine사용
 pipeline = Pipeline([
                      ('cnt_vect', CountVectorizer(stop_words = 'english', ngram_range=(1, 2))), 
                      ('lr_clf', LogisticRegression(C=10))])
@@ -44,34 +33,8 @@ submit6 = pd.read_csv('/sample_submission.csv')
 submit6['sentiment'] = pred
 submit6.head()
 
-
-# In[ ]:
-
-
-pipeline = Pipeline([
-                     ('cnt_vect', TfidfVectorizer(stop_words = 'english', ngram_range=(1, 2))), 
-                     ('lr_clf', LogisticRegression(C=10))])
-
-# Pipeline 객체를 이용해 fit(), predict()로 학습/예측 수행, predict_proba()는 roc_auc 때문에 수행. 
-pipeline.fit(X_train, y_train)
-pred = pipeline.predict(X_test)
-
-submit7 = pd.read_csv('/sample_submission.csv')
-submit7['sentiment'] = pred
-submit7.head()
-
-
-# In[ ]:
-
-
 submit6.to_csv('/baseline_submit10.csv', index=False)
-submit7.to_csv('/baseline_submit11.csv', index=False)
 print('Done')
 
-
-# In[ ]:
-
-
 submit6 = pd.read_csv('/baseline_submit10.csv')
-submit7 = pd.read_csv('/baseline_submit11.csv')
 
