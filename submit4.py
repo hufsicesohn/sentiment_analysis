b21b2bb80462b7535d04e292d168aca36362b4fd
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 18:27:57 +0900

    Update submit4.py
    베르누이 나이브 베이즈 분류 + CountVectorizer

--- submit4.py
+++ submit4.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+# 베르누이 나이브 베이즈 분류 사용
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
@@ -16,19 +11,12 @@ y_train = train['sentiment']
 
 X_test = test['text']
 
-
-# In[ ]:
-
-
+# CountVectorizer로 단어를 벡터로 변환
 vectorizer = CountVectorizer(max_features=5000)
 X_train = vectorizer.fit_transform(X_train).toarray()
 
 bnb = BernoulliNB()
 bnb.fit(X_train, y_train)
-
-
-# In[ ]:
-
 
 X_test = vectorizer.transform(X_test).toarray()
 preds = bnb.predict(X_test)
@@ -37,16 +25,8 @@ submit4 = pd.read_csv('C:/Users/sohnp/Downloads/open (1)/sample_submission.csv')
 submit4['sentiment'] = preds
 submit4.head()
 
-
-# In[ ]:
-
-
 submit4.to_csv('C:/Users/sohnp/baseline_submit6.csv', index=False)
 print('Done')
-
-
-# In[ ]:
-
 
 submit4 = pd.read_csv('C:/Users/sohnp/baseline_submit8.csv')
 
