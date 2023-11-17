1840d3a6de6f9bb1bb271e25007d07b965187291
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 18:32:16 +0900

    Update submit5.py
    첫 전처리 작업 + 베르누이 나이브 베이즈 분류

--- submit5.py
+++ submit5.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+# 전처리 작업, 베르누이 나이브 베이즈 분류 사용
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
@@ -19,21 +14,22 @@ y_train = train['sentiment']
 
 X_test = test['text']
 
-
-# In[ ]:
-
-
+# 전처리 작업
 def preprocess(x):
+  # 불필요한 문자 삭제
   for i in range(32000):
     x[i] = re.sub('[^a-zA-Z]', ' ', x[i])
 
+  # 모든 단어 소문자로 변환
   for i in range(32000):
     x[i] = x[i].lower().split()
 
+  # 불용어 제거
   for i in range(32000):
     stops = set(stopwords.words('english'))
     x[i] = [word for word in x[i] if not word in stops]
 
+  # 단어의 어간 추출
   for i in range(32000):
     stemmer = nltk.stem.SnowballStemmer('english')
     x[i] = [stemmer.stem(word) for word in x[i]]
@@ -43,33 +39,12 @@ def preprocess(x):
 
   return x
 
-
-# In[ ]:
-
-
-def data_processing(text):
-    text = text.lower()
-    text = re.sub(r"https\S+|www|\S+https\S+", '', text, flags=re.MULTILINE)
-    text = re.sub(r'\@w+|\#', '', text)
-    text = re.sub(r'[^\w\s]', '', text)
-    text_tokens = word_tokenize(text)
-    filtered_text = [w for w in text_tokens if not w in stop_words]
-    return " ".join(filtered_text)
-
-
-# In[ ]:
-
-
 X_train = preprocess(X_train)
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train = vectorizer.fit_transform(X_train).toarray()
 
 bnb = BernoulliNB()
 bnb.fit(X_train, y_train)
-
-
-# In[ ]:
-
 
 X_test = preprocess(X_test)
 X_test = vectorizer.transform(X_test).toarray()
@@ -79,16 +54,8 @@ submit5 = pd.read_csv('/sample_submission.csv')
 submit5['sentiment'] = preds
 submit5.head()
 
-
-# In[ ]:
-
-
 submit5.to_csv('C:/Users/sohnp/baseline_submit9.csv', index=False)
 print('Done')
-
-
-# In[ ]:
-
 
 submit5 = pd.read_csv('C:/Users/sohnp/baseline_submit9.csv')
 
